e9f41c08523096ae504d0fa16fa1b5dadfdbdd42
margo <margo@DESKTOP-FOKBQNG>
2023-06-27 17:30:57 +0300

    Добавьте файлы проекта.

---
+++ LVS1.wpf/Main.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LVS1.wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Будет доступно в следующих версиях приложения!");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Будет доступно в следующих версиях приложения!");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Floor floor = new Floor(); 
+            floor.Show();
+            this.Close();
+        }
+    }
+}
---
+++ LVS1.wpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LVS1.wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (login.Text.Length > 0)
+            {
+               
+                    Main wndw = new Main();
+                    wndw.Show();
+                    this.Close();
+                
+            }
+            else
+
+                MessageBox.Show("Проверьте логин или пароль!");
+           
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            Reg reg = new Reg();
+            Reg wndw1 = reg;
+            wndw1.Show();
+            this.Close();
+        }
+
+        private void click_2(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
